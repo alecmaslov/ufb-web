838b1e04087cef263486438ab935c5c529d1a075
Alec Maslov <olemaslo@gmail.com>
2023-01-31 21:12:22 -0800

    value for each cell

--- src/components/BoardTemplate.tsx
+++ src/components/BoardTemplate.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
 import './BoardTemplate.css';
+import { tiles } from './jotunheim.js'
 
 const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
-const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const horizontalAxis = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 export default function BoardTemplate() {
     let board = [];
     for(let A = 0; A < horizontalAxis.length; A++){
         for(let B = 0; B < verticalAxis.length; B++) {
-            board.push(<div className="tile">[{horizontalAxis[A]} {verticalAxis[B]}]</div>)
+            let index = A * verticalAxis.length + B;
+            board.push(<div className="tile" key={index}>{tiles[index].tile}</div>)
         }
     }
     
